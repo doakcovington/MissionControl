c8b36cd97e2f53789496c30be16d15312704506d
doakcovington <doakcovington@protonmail.com>
2020-10-28 18:24:41 -0700

    Created 10 flight control buttons

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -2,8 +2,37 @@ import React from 'react';
 
 const ControlPanel: React.FC = () => {
   return (
-    <div>
-      Hello from ControlPanel Component
+    <div className = "panel">
+      <div className = 'retract'>
+        <button>Retract Arm</button>
+      </div>
+      <div className = 'apu'>
+        <button>Auxiliary Power Units</button>
+      </div>
+      <div className = 'external-tank'>
+        <button>External Tank</button>
+      </div>
+      <div className = 'vent hood'>
+        <button>Vent Hood</button>
+      </div>
+      <div className = 'boil-off-vent'>
+        <button>Boil Off vent</button>
+      </div>
+      <div className = 'tls'>
+        <button>Terminal Launch Sequence</button>
+      </div>
+      <div className = 'srb'>
+        <button>Hydraulic Power Units</button>
+      </div>
+      <div className = 'destruct-system'>
+        <button>Safety Destruct System</button>
+      </div>
+      <div className = 'main-engine'>
+        <button>Main Engine</button>
+      </div>
+      <div className = 'launch'>
+        <button>Launch</button>
+      </div>
     </div>
   )
 }
