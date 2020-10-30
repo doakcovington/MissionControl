8a04c0b0dbcd1645735ac5252aaa37f29931d0e0
doakcovington <doakcovington@protonmail.com>
2020-10-30 12:22:26 -0700

    Removed ControlPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import './App.css';
 import FlightControl from './components/FlightControl';
-import ControlPanel from './components/ControlPanel';
 
 function App() {
   return (
     <div className="App">
       <FlightControl />
-      <ControlPanel />
     </div>
   );
 }
