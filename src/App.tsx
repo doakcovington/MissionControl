e69a9e0fdb4c1092a766f8d97300b69d2fc41aad
doakcovington <doakcovington@protonmail.com>
2020-10-30 12:20:48 -0700

    Added FlightControl Removed CountdownTimer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import './App.css';
-
-import CountdownTimer from "./components/CountdownTimer";
+import FlightControl from './components/FlightControl';
 import ControlPanel from './components/ControlPanel';
 
 function App() {
   return (
     <div className="App">
-      <CountdownTimer />
+      <FlightControl />
       <ControlPanel />
     </div>
   );
