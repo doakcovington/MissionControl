eda3498ac8f2afd6944dd7ba5ed2e74cf4454987
doakcovington <doakcovington@protonmail.com>
2020-10-30 14:32:26 -0700

    Removed CountdownTimer component

--- src/components/FlightControl.tsx
+++ src/components/FlightControl.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import CountdownTimer from './CountdownTimer';
 import ControlPanel from './ControlPanel';
 
 const FlightControl: React.FC = () => {
   return (
     <div>
-      <CountdownTimer timer={60}/>
       <ControlPanel />
     </div>
   )
