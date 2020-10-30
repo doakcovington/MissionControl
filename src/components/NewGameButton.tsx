b1b5783882a029884236f53b82f10cd82af1edb0
doakcovington <doakcovington@protonmail.com>
2020-10-29 19:39:20 -0700

    Built out NewGameButton component

--- src/components/NewGameButton.tsx
+++ src/components/NewGameButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const NewGameButton: React.FC<{status: number}> = ({status}) => {
+const NewGameButton: React.FC<{status?: number}> = ({status}) => {
   return (
     <div>
       {status === 10 ? <button>New Game</button> : null}
