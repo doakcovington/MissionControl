80c4c5b25f7ef64aae704e6c43e352b4b7f0e11f
doakcovington <doakcovington@protonmail.com>
2020-11-02 09:32:19 -0800

    Added instructions

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -6,6 +6,15 @@ const Instructions: React.FC = () => {
     <div>
       <ListGroup>
         <ListGroup.Item>Retract Orbiter Access Arm</ListGroup.Item>
+        <ListGroup.Item>Activate the Auxiliary Power Units (APU)</ListGroup.Item>
+        <ListGroup.Item>Close the Liquid Oxygen Vent on the External Tank</ListGroup.Item>
+        <ListGroup.Item>Raise and Retract the External Tank Vent Hood</ListGroup.Item>
+        <ListGroup.Item>Close the Boil Off Vent</ListGroup.Item>
+        <ListGroup.Item>Start Terminal Launch Sequence</ListGroup.Item>
+        <ListGroup.Item>Activate the Hydraulic Power Units</ListGroup.Item>
+        <ListGroup.Item>Activate the Range Safety Destruct System</ListGroup.Item>
+        <ListGroup.Item>Start Main Engines</ListGroup.Item>
+        <ListGroup.Item>Launch</ListGroup.Item>
       </ListGroup>
     </div>
   )
