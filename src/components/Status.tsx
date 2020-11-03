bf1c48ba9d728b40480a9ca4bfb05f4fc9468323
doakcovington <doakcovington@protonmail.com>
2020-11-02 19:34:18 -0800

    Added border to status cards

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -7,17 +7,21 @@ const Status: React.FC<{report?: boolean}> = ({report}) => {
 
   return (
     <div>
-      <CardDeck>
-        <Card  border="danger" bg={report == true ? "danger" : "light"}>
-          <Card.Body></Card.Body>
-        </Card>
-        <Card  border="warning" bg={report == true ? "warning" : "light"}>
-          <Card.Body></Card.Body>
-        </Card>
-        <Card  border="success" bg={report == true ? "success" : "light"}>
-          <Card.Body></Card.Body>
-        </Card>
-      </CardDeck>
+      <Card border="secondary">
+        <Card.Body>
+          <CardDeck>
+          <Card  border="danger" bg={report == true ? "danger" : "light"}>
+            <Card.Body></Card.Body>
+          </Card>
+          <Card  border="warning" bg={report == true ? "warning" : "light"}>
+            <Card.Body></Card.Body>
+          </Card>
+          <Card  border="success" bg={report == true ? "success" : "light"}>
+            <Card.Body></Card.Body>
+          </Card>
+          </CardDeck>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
