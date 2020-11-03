50df76845b7a4e8b2875a228e19d39cfa04c4417
doakcovington <doakcovington@protonmail.com>
2020-11-03 10:30:18 -0800

    Added start state and button

--- src/components/FlightControl.tsx
+++ src/components/FlightControl.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ControlPanel from './ControlPanel';
 import Instructions from './Instructions';
-import Col from 'react-bootstrap/Col'
+import Col from 'react-bootstrap/Col';
 import { Container } from 'react-bootstrap';
-import Row from 'react-bootstrap/Row'
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 const FlightControl: React.FC = () => {
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <ControlPanel />
-        </Col>
-        <Col>
-          <Instructions />
-        </Col>
-      </Row>
-    </Container>
-  )
+  const [start, setStart] = React.useState(false);
+
+  // return (
+  //   <div>
+  //     <button onClick={() => {setStart(true)}}>Start</button>
+  //     {start === true ?       <Container>
+  //     <Row>
+  //       <Col>
+  //         <ControlPanel />
+  //       </Col>
+  //       <Col>
+  //         <Instructions />
+  //       </Col>
+  //     </Row>
+  //     </Container> : null}
+  //   </div>
+  // )
+  if (start === false) {
+    return (<Button onClick={() =>  {setStart(true)}}>Start</Button>)
+  } else {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              <ControlPanel />
+            </Col>
+            <Col>
+              <Instructions />
+            </Col>
+          </Row>
+      </Container>
+    </div>
+    )
+  }
 }
 
 export default FlightControl;
